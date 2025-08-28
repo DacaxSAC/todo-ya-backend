b7e71c7609d42fe940fefac008a34429f66b46ee
Alexander Saul <alexandersaul2604@gmail.com>
2025-08-28 00:09:31 -0500

    refactor(database): simplify database configuration and update category dto
    Move database config to a direct TypeORM module options object
    Rename createCategory.dto.ts to follow kebab-case convention
    Remove unused imports and commented code in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,11 @@
 import { Module } from '@nestjs/common';
-//import { AppController } from './app.controller';
-//import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-//import { CategoryController } from './category/category.controller';
-//import { CategoryService } from './category/category.service';
 import { CategoryModule } from './modules/category/category.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Database } from './config/database.config';
+import { databaseConfig } from './config/database.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (config: ConfigService<Database>) => ({
-        type: 'postgres',
-        host: config.get('POSTGRES_HOST'),
-        port: config.get('POSTGRES_PORT'),
-        username: config.get('POSTGRES_USER'),
-        password: config.get('POSTGRES_PASSWORD'),
-        database: config.get('POSTGRES_DB'),
-        autoLoadEntities: true,
-        synchronize: true,
-        // entities: [],
-      }),
-      inject: [ConfigService],
-    }),
+     TypeOrmModule.forRoot(databaseConfig),
     CategoryModule,
   ],
   //controllers: [AppController, CategoryController],
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,12 @@
-export interface Database {
-  POSTGRES_HOST: string;
-  POSTGRES_PORT: number;
-  POSTGRES_USER: string;
-  POSTGRES_PASSWORD: string;
-  POSTGRES_DB: string;
-}
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  url: 'postgresql://bd_todo_ya_user:LRIJ8cTFh86xf4vFT8T0pHMcfSpVwDjf@dpg-d2n7h77diees73c9ic3g-a.oregon-postgres.render.com:5432/bd_todo_ya',
+  autoLoadEntities: true,
+  synchronize: true,
+  //dropSchema:true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
